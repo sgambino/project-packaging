f2d4169dec7411500a7467a9b97dc06ea6d763cd
Steve Gambino <sgambino@webassign.net>
2017-11-01 10:16:59 -0400

    Updated packages

--- setup.py
+++ setup.py
@@ -25,22 +25,11 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         "Topic :: Software Development",
         "Topic :: Utilities",
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
@@ -50,11 +39,12 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['date_query']
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["date_query"],
+    # py_modules=["date_query"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
